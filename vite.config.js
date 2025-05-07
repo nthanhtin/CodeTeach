2ec3366f9d7f9e6386e20c41e259f5231f61a4f4
nthanhtin <nthanhtin@gmail.com>
2025-05-07 13:54:28 -0700

    Update package.json and package-lock.json; add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/**/*',
+          dest: 'assets',
+          filter: (filepath) => {
+            return !/\.bak$/.test(filepath) && !/README(\.md)?$/i.test(filepath);
+          }
+        }
+      ]
+    })
+  ]
+});
